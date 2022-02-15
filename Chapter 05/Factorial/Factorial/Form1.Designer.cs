7a93ec2c361544d1286f15dca3739f554a21208f
Corey Hahn <hahnc@charlevoixcounty.org>
2022-02-15 10:19:08 -0500

    Chapter 05 Factorial

---
+++ Chapter 05/Factorial/Factorial/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Factorial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtFactorial = new System.Windows.Forms.TextBox();
+            this.bntCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Factorial:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(61, 12);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(77, 20);
+            this.txtNumber.TabIndex = 2;
+            // 
+            // txtFactorial
+            // 
+            this.txtFactorial.Location = new System.Drawing.Point(61, 57);
+            this.txtFactorial.Name = "txtFactorial";
+            this.txtFactorial.ReadOnly = true;
+            this.txtFactorial.Size = new System.Drawing.Size(161, 20);
+            this.txtFactorial.TabIndex = 3;
+            // 
+            // bntCalculate
+            // 
+            this.bntCalculate.Location = new System.Drawing.Point(61, 100);
+            this.bntCalculate.Name = "bntCalculate";
+            this.bntCalculate.Size = new System.Drawing.Size(75, 23);
+            this.bntCalculate.TabIndex = 4;
+            this.bntCalculate.Text = "&Calculate";
+            this.bntCalculate.UseVisualStyleBackColor = true;
+            this.bntCalculate.Click += new System.EventHandler(this.bntCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(147, 100);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.bntCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(236, 134);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.bntCalculate);
+            this.Controls.Add(this.txtFactorial);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Factorial Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtFactorial;
+        private System.Windows.Forms.Button bntCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Chapter 05/Factorial/Factorial/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Factorial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void bntCalculate_Click(object sender, EventArgs e)
+        {
+            long factorial = 1;
+            int inputNumber = Convert.ToInt32(txtNumber.Text);
+
+            for (; inputNumber > 0; inputNumber--)
+            {
+                factorial *= inputNumber;
+            }
+
+            txtFactorial.Text = factorial.ToString("n0");
+
+            txtNumber.Focus();
+        }
+    }
+}
