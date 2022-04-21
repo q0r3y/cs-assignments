0793408bce22da1b7b9f7f6a1267bde498292930
q0r3y <uxng2a4du6u2@opayq.net>
2022-04-21 10:34:49 -0400

    Fixed operation list

--- CalculatorFinalProject/CalculatorFinalProject/HistoryBox.cs
+++ CalculatorFinalProject/CalculatorFinalProject/HistoryBox.cs
@@ -7,8 +7,34 @@ using System.Threading.Tasks;
 namespace CalculatorFinalProject {
     public class HistoryBox : DisplayBox {
 
+        private string _CurrentNumber = "";
+        public List<string> LastOperation;
+
         public HistoryBox(ref TextBox TextBox) : base(ref TextBox) {
             this.TextBox = TextBox;
+            LastOperation = new List<string>() { "" };
+        }
+
+        public new void HandleKey(string key) {
+            if (int.TryParse(key, out int val)) {
+                _CurrentNumber += val;
+                LastOperation[LastOperation.Count - 1] = _CurrentNumber;
+            }
+            else {
+                _CurrentNumber = "";
+                LastOperation.Add(key);
+                LastOperation.Add(_CurrentNumber);
+            }
+            
+            TextBox.Text = "";
+            foreach (string value in LastOperation) {
+                if (int.TryParse(value, out int result)) {
+                    // Display in the correct MODE
+                    TextBox.Text += value;
+                } else {
+                    TextBox.Text += value;
+                }
+            }
         }
     }
 }
